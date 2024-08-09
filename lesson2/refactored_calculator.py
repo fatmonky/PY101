3deb0774846f0653e6b9a1b9febe40ab89f6b7b8
PJ Teh <peijing.teh.dev@gmail.com>
2024-08-09 11:01:14 +0800

    added calculator bonuses to refactored_calculator.py: it will ask the user for another calculation.

--- lesson2/refactored_calculator.py
+++ lesson2/refactored_calculator.py
@@ -31,6 +31,13 @@ def invalid_operator_check(op):
         op = input()
     return op
 
+def invalid_wish_check(choice):
+    while choice not in ['Y', 'y', 'N', 'n']:
+        prompt("I'm sorry,that's not a valid choice.")
+        prompt("Please key in Y/n: ")
+        choice = input()
+    return choice
+
 # perform calculation, using match-case, and display the result.
 def calculation(numb1, numb2):
     match operator.lower():
@@ -43,19 +50,30 @@ def calculation(numb1, numb2):
         case 'd':
             return numb1 / numb2
 
+calculate = True
 prompt("Welcome to the Calculator!")
-prompt("Enter the first number: ")
-num1 = input()
-num1 = invalid_number_check(num1)
 
-prompt("Enter the second number: ")
-num2 = input()
-num2 = invalid_number_check(num2)
+while calculate:
+    prompt("Enter the first number: ")
+    num1 = input()
+    num1 = invalid_number_check(num1)
 
-prompt("""What type of operation will you like to perform?
-Type\n'a' to add;\n's' to substract;\n'm' to multiply;\n'd' to divide.\n""")
-operator = input()
-operator = invalid_operator_check(operator)
+    prompt("Enter the second number: ")
+    num2 = input()
+    num2 = invalid_number_check(num2)
 
-print(f"""=> The result of your calculation is:
-=>    {calculation(float(num1),float(num2)):.2f}.""")
+    prompt("""What type of operation will you like to perform?
+    Type\n'a' to add;\n's' to substract;\n'm' to multiply;\n'd' to divide.\n""")
+    operator = input()
+    operator = invalid_operator_check(operator)
+
+    print(f"""=> The result of your calculation is:
+    =>    {calculation(float(num1),float(num2)):.2f}.""")
+
+    prompt("Would you like to do another calculation? Y/n")
+    user_wishes = input()
+    user_wishes = invalid_wish_check(user_wishes)
+    if user_wishes not in ['Y', 'y']:
+        calculate = False
+
+prompt("Thank you for using the calculator! See you next time!")
