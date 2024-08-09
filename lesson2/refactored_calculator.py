356e8ff0dad03783a00cac6db898201abf680bfb
PJ Teh <peijing.teh.dev@gmail.com>
2024-08-09 12:27:38 +0800

    completed calculator bonuses.

--- lesson2/refactored_calculator.py
+++ lesson2/refactored_calculator.py
@@ -6,6 +6,9 @@ The use of try-except is interesting, but the use of int(num) doesn't work.
 427pm: linted using pylint: code looks AMAZING... all in neat functions!
 
 9 Aug 24 1113am: linted using pylint again. Now adding JSON functionality.
+1137am: added all messages into calculator_messages.json file, and 
+program works. Needed the hint!
+
 """
 
 import json
@@ -17,26 +20,36 @@ def prompt(message):
     print(f"=> {message}")
 
 def is_valid_number(num):
-    return num.isdigit()
+    return num.isdigit() or isinstance(float(num), float)
 
 def is_valid_operator(op):
     return op.lower() in ['a', 's' ,'m' , 'd']
 
+def invalid_language_check(lang):
+    while lang not in ['e', 'E', 'f', 'F']:
+        prompt(msg_data["invalid_language"])
+        lang = input()
+    if lang in ('f', 'F'):
+        lang = msg_data['fr']
+    else:
+        lang = msg_data['en']
+    return lang
+
 def invalid_number_check(num):
     while is_valid_number(num) is False:
-        prompt(msg_data["invalid_number"])
+        prompt(lg["invalid_number"])
         num = input()
     return num
 
 def invalid_operator_check(op):
     while is_valid_operator(op) is False:
-        prompt(msg_data["invalid_operator"])
+        prompt(lg["invalid_operator"])
         op = input()
     return op
 
 def invalid_wish_check(choice):
     while choice not in ['Y', 'y', 'N', 'n']:
-        prompt(msg_data["invalid_wish"])
+        prompt(lg["invalid_wish"])
         choice = input()
     return choice
 
@@ -52,29 +65,33 @@ def calculation(numb1, numb2):
         case 'd':
             return numb1 / numb2
 
+prompt(msg_data["language"])
+lg = input()
+lg = invalid_language_check(lg)
+
 calculate = True
-prompt(msg_data["welcome"])
+prompt(lg["welcome"])
 
 while calculate:
-    prompt(msg_data["first_number"])
+    prompt(lg["first_number"])
     num1 = input()
     num1 = invalid_number_check(num1)
 
-    prompt(msg_data["second_number"])
+    prompt(lg["second_number"])
     num2 = input()
     num2 = invalid_number_check(num2)
 
-    prompt(msg_data["operation_type"])
+    prompt(lg["operation_type"])
     operator = input()
     operator = invalid_operator_check(operator)
 
-    print(f"""=> The result of your calculation is:
-    =>    {calculation(float(num1),float(num2)):.2f}.""")
+    prompt(lg["result"])
+    print(f"=>    {calculation(float(num1),float(num2)):.2f}.")
 
-    prompt(msg_data["another_calculation"])
+    prompt(lg["another_calculation"])
     user_wishes = input()
     user_wishes = invalid_wish_check(user_wishes)
     if user_wishes not in ['Y', 'y']:
         calculate = False
 
-prompt(msg_data["thank_you"])
+prompt(lg["thank_you"])
