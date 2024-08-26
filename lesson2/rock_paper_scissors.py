6344bf4916e2064d4833680e11ce0a0a78e31f6a
PJ Teh <peijing.teh.dev@gmail.com>
2024-08-26 17:31:45 +0800

    finished rock-paper-scissors, with additional score-tracking. I tried moving lines 83-86 into a separate function, but am running into scope issues, which I'm not sure how to address...; also, Pylint notes that the determine_winner function has too many booleans, but I've the same number of booleans as the example code. -shrug-.

--- lesson2/rock_paper_scissors.py
+++ lesson2/rock_paper_scissors.py
@@ -1,10 +1,6 @@
 """
 Command Line rock-paper-scissors game
 """
-#TODO: pseudocode of logic (done)
-#TODO: write out code (done)
-#TODO: test code through play. (done)
-#TODO: pylint code
 
 import os
 import json
@@ -21,7 +17,7 @@ def prompt(message):
 
 def get_user_choice():
     prompt(MSG_DATA["user_instruction"])
-    choice = input().lower()
+    choice = input().lower().strip()
     while choice not in MSG_DATA["valid_choices"].keys():
         prompt(MSG_DATA["invalid_choice"])
         prompt(MSG_DATA["user_instruction"])
@@ -34,45 +30,40 @@ def display_computer_choice(choice):
 def display_user_choice(choice):
     prompt(f"You chose: {choice.title()}")
 
-def determine_winner(user, computer, user_score, computer_score):
-    # if user has rock and computer has scissors
-    # if user has paper and computer has rock
-    # if user has scissors and computer has paper
-    # user won
+def determine_winner(user, computer):
     if ((user == "rock" and computer == "scissors") or
     (user == "paper" and computer == "rock") or
     (user == "scissors" and computer == "paper")):
         prompt("You have won!")
         return True
-
-    #elif user has rock and computer has paper
-    #elif user has paper and computer has scissors
-    #elif user has scissors and computer has rock
-    # computer won
-    elif ((user == "rock" and computer == "paper") or
+    if ((user == "rock" and computer == "paper") or
     (user == "paper" and computer == "scissors") or
     (user == "scissors" and computer == "rock")):
         prompt("The Computer Won!")
         return False
-
-    # else
-    # it's a tie
-    else:
-        prompt("It's a Tie!")
-        return None
+    prompt("It's a Tie!")
+    return None
 
 
 def get_play_again():
     while True:
-        prompt("Play again? Y/n")
-        play_again = input().lower()
+        prompt(MSG_DATA["play_again"])
+        play_again = input().lower().strip()
         if play_again not in ["Y", "y", "n", "N"]:
             prompt("That's not a valid response.")
-            prompt("Play again? Y/n")
+            prompt(MSG_DATA["invalid_choice"])
+            prompt(MSG_DATA["play_again"])
             play_again = input().lower().strip()
         if play_again in ["Y","y","n","N"]:
             break
     return play_again
+
+def display_scores_farewell(user_score,computer_score):
+    prompt(f"Your final score is {user_score}")
+    prompt(f"vs. the computer score of {computer_score}")
+    prompt("Goodbye!")
+
+
 
 def main():
     clear_screen()
@@ -82,27 +73,22 @@ def main():
         user_choice = get_user_choice()
         user_choice_value = MSG_DATA[
                 "valid_choices"].get(user_choice, "invalid option")
-        #select random computer choice
         computer_choice_value = random.choice(
                 list(MSG_DATA["valid_choices"].values()))
         display_computer_choice(computer_choice_value)
         display_user_choice(user_choice_value)
 
-        # compare user_choice with computer_choice, display winner
-        user_win = determine_winner(user_choice_value, computer_choice_value,
-                         user_score, computer_score)
+        user_win = determine_winner(user_choice_value, computer_choice_value)
+
         if user_win:
             user_score += 1
         elif user_win is False:
             computer_score += 1
 
-        # option to play again
         if get_play_again() in ["N","n"]:
             break
 
     clear_screen()
-    # end and display farewell and score
-    prompt(f"Your final score is {user_score} vs. the computer score of {computer_score}")
-    prompt("Goodbye!")
+    display_scores_farewell(user_score, computer_score)
 
 main()
